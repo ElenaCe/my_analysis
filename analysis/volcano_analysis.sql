f976ed85398102201838dc08cfd8acdfa7c6f431
ElenaCe <47696274+ElenaCe@users.noreply.github.com>
2024-05-21 17:30:36 +0530

    Jupyter notebook to analyse volcano data
    - Looking at countries that have under water volcanos and seeing how many of these compare to above sea level for that specific country

--- analysis/volcano_analysis.sql
+++ analysis/volcano_analysis.sql
@@ -1,39 +1,31 @@
--- select count(*) as volcano_count,country  
--- from volcano_schema.volcano_regional_data 
--- group by country
--- order by volcano_count desc;
-
-
-with 
-sea_level_group as (
-    select 
-        case when elevation_ft < 0 then 'under_sea_level' 
-            when elevation_ft >= 0 then 'above_sea_level'
-            else 'extra'
-        end as sea_level_elevation,
+WITH 
+sea_level_group AS (
+    SELECT 
+        CASE WHEN elevation_ft < 0 THEN 'under_sea_level' 
+             WHEN elevation_ft >= 0 THEN 'above_sea_level'
+             ELSE 'extra'
+        END AS sea_level_elevation,
         country
-    from volcano_schema.volcano_regional_data
+    FROM volcano_schema.volcano_regional_data
 ),
-sea_level_count as (
-    select 
-        count(*) as number_of_each_type,
+sea_level_count AS (
+    SELECT 
+        COUNT(*) AS number_of_each_type,
         sea_level_elevation,
         country
-    from sea_level_group
-    group by sea_level_elevation,country
-    order by number_of_each_type desc
+    FROM sea_level_group
+    GROUP BY sea_level_elevation, country
 ),
-country_select as (
-    select
+country_select AS (
+    SELECT
         country
-    from sea_level_count
-    where number_of_each_type >= 10
+    FROM sea_level_count
+    WHERE number_of_each_type >= 10
 )
-select 
-    number_of_each_type,
-    sea_level_elevation,
-    country
-from sea_level_count
-where country in (select country from country_select);
-
-select * FROM example_table
+SELECT 
+    country,
+    COALESCE(SUM(CASE WHEN sea_level_elevation = 'under_sea_level' THEN number_of_each_type ELSE 0 END), 0) AS under_sea_level_count,
+    COALESCE(SUM(CASE WHEN sea_level_elevation = 'above_sea_level' THEN number_of_each_type ELSE 0 END), 0) AS above_sea_level_count
+FROM sea_level_count
+WHERE country IN (SELECT country FROM country_select)
+GROUP BY country;
