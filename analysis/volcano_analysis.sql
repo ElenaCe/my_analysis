e1776b8bc405b9ef2f05b356295917ed3a8e3a7b
ElenaCe <47696274+ElenaCe@users.noreply.github.com>
2024-05-21 16:00:22 +0530

    Update volcano_analysis.sql
    Analysis on volcano country data

--- analysis/volcano_analysis.sql
+++ analysis/volcano_analysis.sql
@@ -4,9 +4,36 @@
 -- order by volcano_count desc;
 
 
+with 
+sea_level_group as (
+    select 
+        case when elevation_ft < 0 then 'under_sea_level' 
+            when elevation_ft >= 0 then 'above_sea_level'
+            else 'extra'
+        end as sea_level_elevation,
+        country
+    from volcano_schema.volcano_regional_data
+),
+sea_level_count as (
+    select 
+        count(*) as number_of_each_type,
+        sea_level_elevation,
+        country
+    from sea_level_group
+    group by sea_level_elevation,country
+    order by number_of_each_type desc
+),
+country_select as (
+    select
+        country
+    from sea_level_count
+    where number_of_each_type >= 10
+)
 select 
-    *
-from volcano_schema.volcano_regional_data 
-where country = 'United States'
-order by elevation_ft desc;
+    number_of_each_type,
+    sea_level_elevation,
+    country
+from sea_level_count
+where country in (select country from country_select);
 
+select * FROM example_table
