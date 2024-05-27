550ff22fa9af0aeb21b789ca1408cf6e973664a2
ElenaCe <47696274+ElenaCe@users.noreply.github.com>
2024-05-27 17:52:51 +0530

    Change of plan as the dataset is too large
    - The data cannot be saved as it is in the postgres database, as it's too large. I'll analyse the data once it's in a dataframe and summarise it so that it can be used more efficiently while analysing in SQL or being used in Tableau for reporting

--- analysis_db.session.sql
+++
@@ -1,1 +0,0 @@
-select * from ecommerce_schema.oct_nov_19 limit 1;
--- database_create_archive/ecommerce_dataset.py
+++ database_create_archive/ecommerce_dataset.py
@@ -16,7 +16,7 @@ port = os.getenv("DB_PORT")
 
 # Variable names
 schema_name = "ecommerce_schema"
-table_name = "oct_nov_19"
+table_name = "nov_19"
 
 # Create table if not exists function
 def create_table(cursor, schema_name, table_name):
@@ -36,26 +36,18 @@ def create_table(cursor, schema_name, table_name):
     cursor.execute(create_table_query)
     print("Table created successfully!")
 
-# CSV files were deleted from the repo due to the size 
-# They're original source was https://www.kaggle.com/datasets/mkechinov/ecommerce-behavior-data-from-multi-category-store/data
-# Define the base path to your CSV files
+# Path to CSV files
 base_path = '/Users/elenacellitti/Downloads/ecommerce_data/'
-
-# List of file names
 file_names = ['ecommerce_nov_19.csv', 'ecommerce_oct_19.csv']
-
-# Generate full paths
 csv_files = [base_path + file_name for file_name in file_names]
 
 # Read and append CSV files into one DataFrame
 df_list = [pd.read_csv(csv_file) for csv_file in csv_files]
 combined_df = pd.concat(df_list, ignore_index=True)
 
-# Print DataFrame to check before inserting
-print(combined_df.head())
-
 # Connect to PostgreSQL
 conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+conn.autocommit = False  # Ensure autocommit is off
 cursor = conn.cursor()
 
 # Create the table if it doesn't exist
@@ -67,13 +59,15 @@ insert_query = sql.SQL("""
     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
 """).format(sql.Identifier(schema_name), sql.Identifier(table_name))
 
-for index, row in combined_df.iterrows():
-    cursor.execute(insert_query, tuple(row))
-
-conn.commit()
-
-print("Data inserted successfully!")
-
-# Close the cursor and connection
-cursor.close()
-conn.close()
+try:
+    for index, row in combined_df.iterrows():
+        cursor.execute(insert_query, tuple(row))
+    conn.commit()
+    print("Data inserted successfully!")
+except Exception as e:
+    conn.rollback()
+    print(f"An error occurred: {e}")
+finally:
+    # Close the cursor and connection
+    cursor.close()
+    conn.close()
