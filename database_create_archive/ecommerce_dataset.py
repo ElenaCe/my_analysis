f001a212f08cdc21a3bebaeecaf4891511274e12
ElenaCe <47696274+ElenaCe@users.noreply.github.com>
2024-05-28 16:01:28 +0530

    edited query to have better dataset

--- database_create_archive/ecommerce_dataset.py
+++ database_create_archive/ecommerce_dataset.py
@@ -4,19 +4,42 @@ import pandas as pd
 from psycopg2 import sql
 from dotenv import load_dotenv
 
+# Get the csv files
+base_path = '/Users/elenacellitti/Downloads/archive/'
+file_names = ['2019-Oct.csv', '2019-Nov.csv', '2019-Dec.csv']
+csv_files = [base_path + file_name for file_name in file_names]
+
+# Selected columns
+selected_columns = ['event_time','event_type','product_id','price','user_id','user_session']
+
+# List of all the csvs into a DataFrame with only selected columns
+df_list = [pd.read_csv(csv_file, usecols=selected_columns) for csv_file in csv_files]
+
+# Concatenate all to make one dataframe
+combined_df = pd.concat(df_list, ignore_index=True)
+
+# Check dataframe
+print(combined_df.head)
+
+# Connect to postegres to store dataset
+
 # Load .env variables
 load_dotenv()
 
-# Details to connect to PostgreSQL
+# Details to connect
 dbname = os.getenv("DB_NAME")
 user = os.getenv("DB_USER")
 password = os.getenv("DB_PASSWORD")
 host = os.getenv("DB_HOST")
 port = os.getenv("DB_PORT")
 
+# Connect to PostgreSQL
+conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+cursor = conn.cursor()
+
 # Variable names
 schema_name = "ecommerce_schema"
-table_name = "nov_19"
+table_name = "oct_dec_19"
 
 # Create table if not exists function
 def create_table(cursor, schema_name, table_name):
@@ -25,9 +48,6 @@ def create_table(cursor, schema_name, table_name):
             event_time TIMESTAMP NOT NULL,
             event_type VARCHAR,
             product_id VARCHAR,
-            category_id VARCHAR,
-            category_code VARCHAR,
-            brand VARCHAR,
             price INT,
             user_id VARCHAR,
             user_session VARCHAR
@@ -36,27 +56,13 @@ def create_table(cursor, schema_name, table_name):
     cursor.execute(create_table_query)
     print("Table created successfully!")
 
-# Path to CSV files
-base_path = '/Users/elenacellitti/Downloads/ecommerce_data/'
-file_names = ['ecommerce_nov_19.csv', 'ecommerce_oct_19.csv']
-csv_files = [base_path + file_name for file_name in file_names]
-
-# Read and append CSV files into one DataFrame
-df_list = [pd.read_csv(csv_file) for csv_file in csv_files]
-combined_df = pd.concat(df_list, ignore_index=True)
-
-# Connect to PostgreSQL
-conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
-conn.autocommit = False  # Ensure autocommit is off
-cursor = conn.cursor()
-
 # Create the table if it doesn't exist
 create_table(cursor, schema_name, table_name)
 
 # Insert data into the SQL table
 insert_query = sql.SQL("""
-    INSERT INTO {}.{} (event_time, event_type, product_id, category_id, category_code, brand, price, user_id, user_session)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+    INSERT INTO {}.{} (event_time, event_type, product_id,price, user_id, user_session)
+    VALUES (%s, %s, %s, %s, %s, %s)
 """).format(sql.Identifier(schema_name), sql.Identifier(table_name))
 
 try:
